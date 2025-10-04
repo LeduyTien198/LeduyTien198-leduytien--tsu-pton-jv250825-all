43a7b8836ed582218e5e809dc1f3712705db8d9c
LeduyTien198 <leduytien611@gmail.com>
2025-10-04 17:25:34 +0900

    Add files via upload

---
+++ lession13/bai1/js.js
@@ -0,0 +1,8 @@
+// Thực hiện tạo mảng và sử dụng các phương thức thêm phần tử vào mảng các phần tử ngẫu nhiên làm sao cho mảng đủ 10 phần tử, sau đó hiển thị mảng ra màn hình
+
+
+const number = [];
+for (i = 1; i < 10; i++) {
+    number.push(Math.floor(Math.random() * 100));
+}
+console.log(number);
